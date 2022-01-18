33e961cd3763a373e77463aba3c927ab4cd15971
“nursultan <nursultan.meimanbekuulu.on@gmail.com>
2022-01-19 01:24:18 +0600

    add task3

---
+++ js/task3.js
@@ -0,0 +1,3 @@
+let souvenir = Number(prompt("сколько сувенир купили?"));
+let accessories = Number(prompt("сколько аксессуар купили?"));
+alert("общий вес: " + (souvenir * 75 + accessories * 112) / 1000 + " kg");
